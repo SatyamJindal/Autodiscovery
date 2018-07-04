ddb65619d700a1ec992645ccb9a6202a947f5527
Satyam Jindal <satyamjindal072@gmail.com>
2018-07-04 16:55:30 +0530

    Update Setup.py

--- Setup.py
+++ Setup.py
@@ -1,4 +1,17 @@
-from cx_Freeze import setup, Executable
+'''
+
+Made by:-
+-----------------
+SATYAM JINDAL
+04-07-2018
+-----------------
+
+
+'''
+
+# **cx_Freeze** used to create the Executable File for the client side.
+
+from cx_Freeze import setup, Executable                                                     
 
 setup(name='try',
       version='0.1',
