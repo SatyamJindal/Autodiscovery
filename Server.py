82dbb332f15e17f739f7049e2c4aa293a8b2bcb8
Satyam Jindal <satyamjindal072@gmail.com>
2018-07-22 00:34:11 +0530

    Update Server.py

--- Server.py
+++ Server.py
@@ -90,7 +90,7 @@ def server_program():
       getinds(mine,inds)   
       softs = mine[indi:]                                                                             # Contains the List of all softwares
       
-      cursor.execute('SELECT MAX(id) from text;')                                                     # Gets the previous maximum Id so a duplicate key is not created
+      cursor.execute('SELECT MAX(id) from text;')                                                     # Gets the precious maximum Id so a duplicate key is not created
                                                                                                       # Done to avoid the unique primary key law
       num = cursor.fetchall()                                                          
       num=str(num[0])
@@ -107,4 +107,5 @@ def server_program():
 
 
 if __name__ == '__main__':
-    server_program()
+   print('Server is running')
+   server_program()
